ef62ceb194c809eaca2e86f1cc4cbbdba54af74a
primys <a2uso@tuta.io>
2019-09-14 05:46:48 -0700

    Add View and ExplorerView parents

---
+++ src/main/kotlin/io/usoamic/explorer/base/ExplorerView.kt
@@ -0,0 +1,97 @@
+package io.usoamic.explorer.base
+
+import io.usoamic.usoamickotlinjs.core.Usoamic
+import io.usoamic.usoamickotlinjs.core.extension.getTransactionsByAddress
+import io.usoamic.usoamickotlinjs.model.Transaction
+import io.usoamic.usoamickotlinjs.other.Config
+import io.usoamic.usoamickotlinjs.other.Config.Companion.CONTRACT_ABI
+import io.usoamic.usoamickotlinjs.other.Config.Companion.NODE
+import io.usoamic.web3kt.core.Web3
+import io.usoamic.web3kt.core.contract.model.CallOption
+import io.usoamic.web3kt.core.contract.util.Coin
+import io.usoamic.web3kt.core.extension.newContract
+import io.usoamic.web3kt.tx.block.DefaultBlockParameterName
+import io.usoamic.web3kt.util.EthUnit
+import io.usoamic.explorer.enumcls.Page
+import io.usoamic.explorer.other.Timestamp
+import js.externals.jquery.extension.onClick
+import js.externals.jquery.jQuery
+import kotlin.browser.localStorage
+
+abstract class ExplorerView(application: Application) : View(application) {
+    private val web3 = Web3(NODE)
+    private val contract = web3.newContract<Usoamic>(CONTRACT_ABI, "0x42210806DCA8E0C7A5Cff83192852eB0db4ce764")
+    private val methods = contract.methods
+
+    private val logoutBtn = jQuery("#logout")
+    private val callOption = CallOption("0x5d8766ac0075bdf81b48f0bfcf92449e9def0f37 ")
+
+    init {
+        setListeners()
+    }
+
+    private fun setListeners() {
+        logoutBtn.onClick {
+//            application.openPage(Page.FIRST)
+        }
+    }
+
+    protected fun getEthBalance(address: String, callback: (String) -> Unit) {
+        web3.eth.getBalance(address, DefaultBlockParameterName.LATEST)
+            .then {
+                callback(web3.utils.fromWei(it, EthUnit.ETHER))
+            }
+            .catch {
+                onException(it)
+            }
+    }
+
+    protected fun getUsoBalance(address: String, callback: (Coin) -> Unit) {
+        methods.balanceOf(address).call(callOption)
+            .then {
+                callback(Coin.fromSat(it))
+            }
+            .catch {
+                onException(it)
+            }
+    }
+
+    protected fun getTransactions(lastId: Long?, loadedLastId: Long, callback: (List<List<Any>>) -> Unit) {
+        /*
+        lastId?.let { lId ->
+            methods.getTransactionsByAddress(address, lId, loadedLastId) { list: MutableList<Transaction>, throwable: Throwable?, hasUpdate: Boolean ->
+                if(!hasUpdate) {
+                    return@getTransactionsByAddress
+                }
+                if(throwable != null) {
+                    onException(throwable)
+                    return@getTransactionsByAddress
+                }
+                val txList = mutableListOf<List<Any>>()
+                var id = list.size
+
+                list.forEach { tx ->
+                    val txType = TxUtils.getTxType(address, tx.from, tx.to)
+                    txList.add(
+                        listOf(
+                            id,
+                            txType.toPlainString(),
+                            when (txType) {
+                                Transfer.DEPOSIT -> tx.from
+                                Transfer.WITHDRAWAL -> tx.to
+                                Transfer.INTERNAL -> address
+                                Transfer.UNKNOWN -> "N/A"
+                            },
+                            Coin.fromSat(tx.value).toPlainString(),
+                            Timestamp.fromBigNumber(tx.timestamp).toLocaleString()
+                        )
+                    )
+                    id--
+                }
+                callback(txList)
+            }
+        }
+
+         */
+    }
+}
---
+++ src/main/kotlin/io/usoamic/explorer/base/View.kt
@@ -0,0 +1,49 @@
+package io.usoamic.explorer.base
+
+import js.externals.jquery.JQuery
+import js.externals.jquery.extension.clearText
+import js.externals.jquery.extension.clearVal
+import org.w3c.dom.HTMLElement
+
+abstract class View(protected val application: Application) {
+    abstract val view: JQuery<HTMLElement>
+    abstract val navBarItem: JQuery<HTMLElement>?
+
+    open fun startLoading() {
+        application.startLoading()
+    }
+
+    open fun stopLoading() {
+        application.stopLoading()
+    }
+
+    open fun onStart() {
+        view.show()
+    }
+
+    open fun onRefresh() { }
+
+    open fun onStop() {
+        view.hide()
+    }
+
+    protected fun clearInputs(list: List<JQuery<HTMLElement>>) {
+        list.forEach {
+            it.clearVal()
+        }
+    }
+
+    protected fun clearTexts(list: List<JQuery<HTMLElement>>) {
+        list.forEach {
+            it.clearText()
+        }
+    }
+
+    open fun onError(s: String?) {
+        application.onError(s)
+    }
+
+    open fun onException(t: Throwable) {
+        application.onException(t)
+    }
+}
