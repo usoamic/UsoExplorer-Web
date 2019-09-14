866a8abdfd709e27cd1f59e21d1bb37ed7adfafd
primys <a2uso@tuta.io>
2019-09-14 09:21:10 -0700

    Add code for filling table

--- src/main/kotlin/io/usoamic/explorer/view/TransfersView.kt
+++ src/main/kotlin/io/usoamic/explorer/view/TransfersView.kt
@@ -2,8 +2,14 @@ package io.usoamic.explorer.view
 
 import io.usoamic.explorer.base.Application
 import io.usoamic.explorer.base.ExplorerView
+import io.usoamic.explorer.other.Timestamp
+import io.usoamic.explorer.util.CommonUtils
 import io.usoamic.web3kt.core.contract.util.Coin
+import js.externals.datatables.net.JQueryDataTable
+import js.externals.datatables.net.extension.dataTable
+import js.externals.datatables.net.model.DataTableOption
 import js.externals.jquery.jQuery
+import kotlin.math.min
 
 class TransfersView(application: Application) : ExplorerView(application) {
     override val view = jQuery("#add_wallet_view")
@@ -13,8 +19,11 @@ class TransfersView(application: Application) : ExplorerView(application) {
     private val usoSupply = jQuery("#uso_supply")
     private val usoFrozen = jQuery("#uso_frozen")
 
+    private val lastTransfersTable = jQuery("#last_transfers").unsafeCast<JQueryDataTable>()
+    private var numberOfLastTransfers: Long = 0
+
     init {
-        setListeners()
+        prepareLastTransfers()
     }
 
     override fun onStart() {
@@ -27,6 +36,7 @@ class TransfersView(application: Application) : ExplorerView(application) {
         refreshEthHeight()
         refreshUsoSupply()
         refreshUsoFrozen()
+        refreshLastTransfers()
     }
 
     private fun refreshEthHeight() {
@@ -48,27 +58,21 @@ class TransfersView(application: Application) : ExplorerView(application) {
     private fun refreshUsoFrozen() {
         methods.getFrozen().call(callOption)
             .then {
-                usoFrozen.text(if(it) "YES" else "NO")
+                usoFrozen.text(if (it) "YES" else "NO")
             }
             .catch(this::onException)
     }
 
+    private fun prepareLastTransfers() {
+        lastTransfersTable.dataTable(DataTableOption.initLoading())
+    }
 
-    private fun setListeners() {
-//        saveBtn.onClick {
-//            startLoading()
-//            try {
-//                val privateKey = privateKeyInput.content().removeHexPrefixIfExist()
-//                val password = passwordInput.content()
-//                val confirmPassword = confirmPasswordInput.content()
-//
-//
-//
-//            } catch (e: Throwable) {
-//                saveBtn.stopLoading(false)
-//                toastr.error(e.message)
-//            }
-//        }
+    private fun refreshLastTransfers() {
+        getTransactions {
+            numberOfLastTransfers = it.size.toLong()
+            val callback =
+            lastTransfersTable.dataTable(DataTableOption(data = it))
+        }
     }
 
     override fun onStop() {
@@ -76,11 +80,56 @@ class TransfersView(application: Application) : ExplorerView(application) {
         summary.hide()
     }
 
+    private fun getTransactions(callback: (MutableList<List<Any>>) -> Unit) {
+        methods.getNumberOfTransactions()
+            .call(callOption)
+            .then {
+                val lastId = min(30, it.toLong())
+                if (numberOfLastTransfers == lastId) {
+                    return@then
+                }
+                if (lastId > 0) {
+                    iterateTransactions(mutableListOf(),  lastId,0, lastId, callback)
+                } else {
+                    callback(mutableListOf())
+                }
+            }
+            .catch(this::onException)
+    }
+
+    private fun iterateTransactions(
+        list: MutableList<List<Any>>,
+        id: Long,
+        txId: Long,
+        lastId: Long,
+        callback: (MutableList<List<Any>>) -> Unit
+    ) {
+        methods.getTransaction(txId.toString()).call(callOption)
+            .then { tx ->
+                list.add(
+                    listOf(
+                        "$id",
+                        CommonUtils.reduceString(tx.from, 15),
+                        CommonUtils.reduceString(tx.to, 15),
+                        Coin.fromSat(tx.value).toPlainString(),
+                        Timestamp.fromBigNumber(tx.timestamp).toLocaleString()
+                    )
+                )
+                val nextId = (txId + 1)
+                if (nextId < lastId) {
+                    iterateTransactions(list, (id - 1), nextId, lastId, callback)
+                } else {
+                    callback(list)
+                }
+            }
+            .catch(this::onException)
+    }
+
     companion object {
         private var instance: TransfersView? = null
 
         fun open(application: Application) {
-            if(instance == null) {
+            if (instance == null) {
                 instance = TransfersView(application)
             }
             return application.open(instance!!)
