2dbee6b585408b864ac285996e8f0e73a40f4922
primys <a2uso@tuta.io>
2019-09-15 08:55:03 -0700

    Move code from ExplorerView to View and remove ExplorerView

--- src/main/kotlin/io/usoamic/explorer/base/View.kt
+++ src/main/kotlin/io/usoamic/explorer/base/View.kt
@@ -1,19 +1,28 @@
 package io.usoamic.explorer.base
 
+import io.usoamic.usoamickotlinjs.core.Usoamic
+import io.usoamic.usoamickotlinjs.other.Config
+import io.usoamic.web3kt.core.Web3
+import io.usoamic.web3kt.core.contract.model.CallOption
+import io.usoamic.web3kt.core.extension.newContract
 import js.externals.jquery.JQuery
-import js.externals.jquery.extension.clearContent
-import js.externals.jquery.extension.clearText
-import js.externals.jquery.extension.clearVal
+import js.externals.jquery.extension.*
 import org.w3c.dom.HTMLElement
 
 abstract class View(protected val application: Application) {
+    abstract val navBarItem: JQuery<HTMLElement>?
     abstract val view: JQuery<HTMLElement>
     var isFind: Boolean = false
     abstract val input: JQuery<HTMLElement>
     abstract val searchBtn: JQuery<HTMLElement>
 
+    protected val web3 = Web3(Config.NODE)
+    private val contract = web3.newContract<Usoamic>(Config.CONTRACT_ABI, Config.CONTRACT_ADDRESS)
+    protected val methods = contract.methods
+    protected val callOption = CallOption("0x8b27fa2987630a1acd8d868ba84b2928de737bc2")
+
     open fun startLoading() {
-        application.startLoading()
+        searchBtn.startLoading()
     }
 
     open fun onFind(findData: String) {
@@ -21,7 +30,7 @@ abstract class View(protected val application: Application) {
     }
 
     open fun stopLoading() {
-        application.stopLoading()
+        searchBtn.stopLoading()
     }
 
     open fun onStart() {
